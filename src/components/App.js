a8995713afdcee71c6d1b4773b9e2015b6dd2ffd
Laura Vieira <lauramvieira.aus@gmail.com>
2020-10-26 18:38:53 -0300

    first configs

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+
+
+export default function App () {
+    return (
+        <h1>teste</h1>
+    );
+}
